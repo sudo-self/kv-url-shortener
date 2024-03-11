b59fa602c1c5b7bc0e2595c061e81d464e9fd84c
Sudo-Self <sudo-self@proton.me>
2024-03-10 23:12:31 -0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,30 +1,24 @@
 // server.ts
 
-// deno run -A --unstable server.ts
-
-// localhost:8000
-
-
-
 const kv = await Deno.openKv();
 
 Deno.serve(async (request: Request) => {
-  // Create short links
   if (request.method == "POST") {
+    // Handle POST request to create short links
     const body = await request.text();
     const { slug, url } = JSON.parse(body);
     const result = await kv.set(["links", slug], url);
     return new Response(JSON.stringify(result));
-  }
-
-  // Redirect short links
-  const slug = request.url.split("/").pop() || "";
-  const url = (await kv.get(["links", slug])).value as string;
-  if (url) {
-    return Response.redirect(url, 301);
   } else {
-    const m = !slug ? "Please provide a slug." : `Slug "${slug}" not found`;
-    return new Response(m, { status: 404 });
+    // Handle GET request to redirect short links
+    const slug = request.url.split("/").pop() || "";
+    const url = (await kv.get(["links", slug]))?.value as string;
+    if (url) {
+      return Response.redirect(url, 301);
+    } else {
+      const message = !slug ? "Please provide a slug." : `Slug "${slug}" not found`;
+      return new Response(message, { status: 404 });
+    }
   }
 });
 
